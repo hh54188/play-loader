11b7c2cc883165e4c1ace20b167c132d0f633ef2
invegas <juststayinvegas@gmail.com>
2014-04-18 01:05:02 +0800

    update

--- js/requirejs/main.js
+++ js/requirejs/main.js
@@ -1,7 +1,3 @@
 require(["dep_A", "dep_B", "dep_C"], function(A, B, C) {
 
-    (function(second) {
-        var start = +new Date();
-        while (start + second * 1000 > +new Date()) {}
-    })(window.EXE_TIME);
 });
--- js/seajs/main.js
+++ js/seajs/main.js
@@ -2,12 +2,5 @@ define(function(require, exports, module) {
 
     var mod_A = require("dep_A");
     var mod_B = require("dep_B");
-    // var mod_C = require("dep_C");
-    document.body.onclick = function() {
-        var mod_C = require("dep_C");
-    }
-
-    module.exports = function() {
-
-    }
+    var mod_C = require("dep_C");
 });
