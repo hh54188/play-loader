d9f27649148667c172884c22c928f7247d3aa763
hh54188 <252438823@qq.com>
2014-04-30 13:59:42 +0800

    seajs could execute define without factory

--- js/seajs/main.js
+++ js/seajs/main.js
@@ -1,6 +1,6 @@
-define(function(require, exports, module) {
+define(["dep_A", "dep_B", "dep_C"], function(require, exports, module) {
 
-    var mod_A = require("dep_A");
-    var mod_B = require("dep_B");
-    var mod_C = require("dep_C");
+    // var mod_A = require("dep_A");
+    // var mod_B = require("dep_B");
+    // var mod_C = require("dep_C");
 });
